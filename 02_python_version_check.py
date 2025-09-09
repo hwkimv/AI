60f573b07376a9ce17d3f0516c8d8ff9362de502
김한욱 <hwkimv72@gmail.com>
2025-09-09 11:19:59 +0900

    week1

---
+++ 02_python_version_check.py
@@ -0,0 +1,28 @@
+print("2324018")
+
+import sys
+print('python', sys.version)
+
+import numpy
+print('numpy', numpy.__version__)
+
+import scipy
+print('scipy', scipy.__version__)
+
+import matplotlib
+print('matplotlib', matplotlib.__version__)
+
+import sklearn
+print('sklearn', sklearn.__version__)
+
+import PIL
+print('PIL', PIL.__version__)
+
+import bs4
+print('bs4', bs4.__version__)
+
+import pydot
+print('pydot', pydot.__version__)
+
+import tensorflow
+print('tensorflow', tensorflow.__version__)
